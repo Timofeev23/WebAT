e5913395daafaf9436b51e252ad4ac8a90d8720f
Anton Timofeev <Timofeev85@gmail.com>
2023-09-09 22:08:55 +0300

    Новый проект

--- WebTest1.py
+++ WebTest1.py
@@ -1,1 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+#service = Service(executable_path='C:\Users\Admin\WebAT\chromedriver')  # указываем путь до драйвера
+browser = webdriver.Chrome()
+
+url = "https://ya.ru"
+
+try:
+    browser.get(url=url)
+    
+except:
+
+finally:
+    browser.close()
+    browser.quit()
+
 
